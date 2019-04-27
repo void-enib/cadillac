c9a7499309db8dca3f2cddbeaed755c22980819c
Arcadeuh <47535482+Arcadeuh@users.noreply.github.com>
2019-04-27 11:42:54 +0200

    test_cadillac_1.0
    Version 1 du prototype (avec mitraillette presque fonctionnelle

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public float fireRate;
+    public Transform bullet;
+    public Transform spawnBullet;
+    public int munition;
+    private float nextFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1") && Time.time > nextFire && munition!=0)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(bullet, spawnBullet.position, spawnBullet.rotation);
+            munition--;
+        }
+    }
+}
