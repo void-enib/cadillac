a78ec505afabc9c0e5c57955845e2a0605579aaa
Arcadeuh <47535482+Arcadeuh@users.noreply.github.com>
2019-05-05 16:46:22 +0200

    Mitraillette
    Les balles de la mitraillette prennent la direction voulue, mais elles ne se déplacent pas

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -5,26 +5,60 @@ using UnityEngine;
 public class GunController : MonoBehaviour
 {
     public float fireRate;
-    public Transform bullet;
+    public GameObject bullet;
     public Transform spawnBullet;
     public int munition;
+    public float inaccuracy;
+    public float speed;
+
+    private Vector2 movement;
     private float nextFire;
+    private float randomAngle;
+    private Rigidbody2D rbBullet;
+    private GameObject rb;
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    
+
+    void setBullet()
+    {
+        randomAngle = Random.Range(-inaccuracy, inaccuracy);
+        movement = new Vector2(randomAngle, speed);
+        Debug.Log("x = " + movement.x);
+        Debug.Log("y = " + movement.y);
+        Rigidbody2D rbBullet = bullet.GetComponent<Rigidbody2D>();
+        GameObject copyBullet = Instantiate(bullet, spawnBullet.position, spawnBullet.rotation);
+        Rigidbody2D rbCopyBullet = GetComponent<Rigidbody2D>();
+        rbCopyBullet.velocity = movement;
+        Debug.Log("rb.velocity = " + rbCopyBullet.velocity);
+    }
+
+    void Fire()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire && munition != 0)
+        {
+            nextFire = Time.time + fireRate;
+            setBullet();
+            munition--;
+        }
+    }
+
+    void Target()
+    {
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButton("Fire1") && Time.time > nextFire && munition!=0)
-        {
-            nextFire = Time.time + fireRate;
-            Instantiate(bullet, spawnBullet.position, spawnBullet.rotation);
-            munition--;
-        }
+        Fire();
+        Target();
     }
 }
